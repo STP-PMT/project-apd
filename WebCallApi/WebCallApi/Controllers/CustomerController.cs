642274bdfa9cbc9c0f3a29ffc1e33c69641b7941
STP-PMT <62011212109@msu.ac.th>
2021-10-29 19:19:01 +0700

    done

--- WebCallApi/WebCallApi/Controllers/CustomerController.cs
+++ WebCallApi/WebCallApi/Controllers/CustomerController.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using WebCallApi.Models;
 
@@ -48,7 +49,7 @@ namespace WebCallApi.Controllers
         }
 
         // GET: CustomerController/Create
-        public ActionResult Create()
+        public IActionResult Create()
         {
             return View();
         }
@@ -56,43 +57,76 @@ namespace WebCallApi.Controllers
         // POST: CustomerController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public async Task<IActionResult> Create(Customer customer)
         {
-            try
+            Customer cus = new Customer();
+            using (var httpClient = new HttpClient(_clientHandler))
             {
-                return RedirectToAction(nameof(Index));
+                StringContent content = new 
+                    StringContent(JsonConvert.SerializeObject(customer),Encoding.UTF8,"application/json");
+                using (var reponse = await httpClient.PostAsync("https://localhost:44383/api/Customer", content))
+                {
+                    string apiResponse = await reponse.Content.ReadAsStringAsync();
+                    cus = JsonConvert.DeserializeObject<Customer>(apiResponse);
+                    if (ModelState.IsValid) {
+                        return RedirectToAction(nameof(Index));
+                    }
+                }
             }
-            catch
-            {
-                return View();
-            }
+            return View(cus);           
         }
 
         // GET: CustomerController/Edit/5
-        public ActionResult Edit(int id)
+        public async Task<IActionResult> Edit(int id)
         {
-            return View();
+            Customer  mycus = null;
+            cuslist = new List<Customer>();
+            using (var httpClient = new HttpClient(_clientHandler))
+            {
+                using (var response = await httpClient.GetAsync("https://localhost:44383/api/Customer/" + id))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    cuslist = JsonConvert.DeserializeObject<List<Customer>>(apiResponse);
+                    mycus = cuslist[0];
+                   
+                }
+            }
+            return View(mycus);
+           
         }
 
         // POST: CustomerController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public async Task<IActionResult> Edit(int id, Customer customer)
         {
-            try
+            Customer cus = new Customer();
+            if (!ModelState.IsValid) return View(customer);
+            using(var httpClient = new HttpClient(_clientHandler))
             {
-                return RedirectToAction(nameof(Index));
+                StringContent content =
+                    new StringContent(JsonConvert.SerializeObject(customer), Encoding.UTF8, "application/json");
+                using (var response = await httpClient.PutAsync("https://localhost:44383/api/Customer/" + id, content))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    cus = JsonConvert.DeserializeObject<Customer>(apiResponse);
+                }
             }
-            catch
-            {
-                return View();
-            }
+            return RedirectToAction(nameof(Index));
         }
 
         // GET: CustomerController/Delete/5
-        public ActionResult Delete(int id)
+        public async Task<IActionResult> Delete(int id)
         {
-            return View();
+            string del = "";
+            using (var httpClient = new HttpClient(_clientHandler))
+            {
+                using (var reponse = await httpClient.DeleteAsync("https://localhost:44383/api/Customer/" + id))
+                {
+                    del = await reponse.Content.ReadAsStringAsync();
+                }
+            }
+            return RedirectToAction(nameof(Index));
         }
 
         // POST: CustomerController/Delete/5
